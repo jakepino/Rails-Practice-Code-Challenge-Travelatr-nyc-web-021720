3239c481fc37d66cf56b0b53a12baaeead4fa712
anoorzi <anoorzi123@gmail.com>
2020-03-18 14:19:26 -0400

    blogger update

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,2 +1,4 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-
+    belongs_to :post
+    belongs_to :blogger
 end
